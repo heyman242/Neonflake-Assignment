12b880ff82e1ed8577da8738f7098ce0146aebd5
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-26 02:50:38 +0530

    backend added user routes

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 const app = express();
 
@@ -12,7 +15,11 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", authenticateUser, userRouter);
 
 const port = process.env.PORT || 5100;
 try {
