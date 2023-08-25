faf675f4f1421704d799577ff3595067ac81869b
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-26 02:09:29 +0530

    backend added auth routes and api for login, register logout

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 
+import authRouter from "./routes/authRouter.js";
+
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use("/api/v1/auth", authRouter);
 
 const port = process.env.PORT || 5100;
 try {
