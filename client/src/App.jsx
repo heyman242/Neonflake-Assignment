502f8feda640f50390ddb75ba504970ad36b9c9b
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-26 00:40:39 +0530

    frontend basic routes setup and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+        
+      },
+      {
+        path: "login",
+        element: <Login />,
+        
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+        
+        children: [
+          {
+            index: true,
+            element: <MainPage />,
+          },
+          {
+            path:"upload/list",
+            element:<SecondPage/>
+          },
+          {
+            path:"upload/detail",
+            element:<ThirdPage/>
+          }
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
