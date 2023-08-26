039730677a1373554b4af65d619e5e4198171c94
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-26 20:42:23 +0530

    frontend updated secondpage css

--- client/src/pages/SecondPage.jsx
+++ client/src/pages/SecondPage.jsx
@@ -24,8 +24,6 @@ const SecondPage = () => {
       .get(`/api/v1/user/uploads/${jobId}`)
       .then((response) => {
         if (response.status === 200) {
-          const videoUrl = response.data.videoUrl;
-          // Redirect to ThirdPage with the jobId as a route parameter
           navigate(`/dashboard/upload/${jobId}`);
         }
       })
@@ -35,22 +33,45 @@ const SecondPage = () => {
   };
 
   return (
-    <div>
-      <h2>Uploaded Files</h2>
-      <ul>
-        {uploads.map((upload, index) => (
-          <li key={index}>
-            <strong>Title:</strong> {upload.title}
-            <br />
-            <strong>Description:</strong> {upload.description}
-            <br />
-            <strong>Thumbnail URL:</strong>{" "}
-            <img src={upload.thumbnailUrl} alt="Thumbnail" />
-            <br />
-            <button onClick={() => watchVideo(upload._id)}>Watch</button>
-          </li>
-        ))}
-      </ul>
+    <div className="max-w-5xl mx-auto p-6 rounded-lg shadow-lg border border-gray-300">
+      <h2 className="text-2xl font-bold mb-4 text-center underline">
+        Uploaded Files
+      </h2>
+      <table className="min-w-full border-collapse border border-gray-300">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="p-2 text-left">Title</th>
+            <th className="p-2 text-left">Description</th>
+            <th className="p-2 text-left">Thumbnail</th>
+            <th className="p-2 text-left">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {uploads.map((upload, index) => (
+            <tr key={index} className="border-t border-gray-300">
+              <td className="p-2 text-lg font-semibold">{upload.title}</td>
+              <td className="p-2 text-lg font-semibold">
+                {upload.description}
+              </td>
+              <td className="p-2">
+                <img
+                  src={upload.thumbnailUrl}
+                  alt="Thumbnail"
+                  className="w-30 h-20 object-cover"
+                />
+              </td>
+              <td className="p-2">
+                <button
+                  className="bg-blue-500 text-white px-3 py-1 rounded-lg hover:bg-blue-600 transition-colors"
+                  onClick={() => watchVideo(upload._id)}
+                >
+                  Watch
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
