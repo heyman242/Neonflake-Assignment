f63d6b3e09c8c18ca5f28733d4686320d7d12069
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-27 00:18:50 +0530

    ready

--- server.js
+++ server.js
@@ -22,12 +22,28 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", authenticateUser, userRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
 
 const port = process.env.PORT || 5100;
 try {
